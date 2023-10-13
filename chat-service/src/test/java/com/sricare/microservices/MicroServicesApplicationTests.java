aeef4945da73d216cd6bb1185835651ac4247f07
Sachin Umayangana <sachinumayangana@gmail.com>
2023-10-14 02:38:20 +0530

    Chat integration

---
+++ chat-service/src/test/java/com/sricare/microservices/MicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sricare.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
