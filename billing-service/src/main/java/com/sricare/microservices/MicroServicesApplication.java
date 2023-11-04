1e398f4c09d73a4c7e676bc5f95e3fd5b12fa961
Danodya Supun <danodyasupun7@gmail.com>
2023-11-04 22:31:42 +0530

    Billing service modified.

--- billing-service/src/main/java/com/sricare/microservices/MicroServicesApplication.java
+++ billing-service/src/main/java/com/sricare/microservices/MicroServicesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication
 @EnableDiscoveryClient
 public class MicroServicesApplication {
 
